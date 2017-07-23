1b28dfca94b4ed248dde433ec0d7ae0728a6b437
aoao2 <moppet.tw@gmail.com>
2017-07-23 09:42:29 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,13 +4,21 @@
 
     <h1>タスク新規作成ページ</h1>
     
-    {!! Form::model($taskC, ['route' => 'task_l.store']) !!}
-    
-        {!! Form::label('content', 'タスク') !!}
-        {!! Form::text('content') !!}
-        {!! Form::select('status', ['済' => '済', '未' => '未', '保留' => '保留'], null, ['placeholder' => '']) !!}
-        {!! Form::submit('投稿') !!}
-    
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+        {!! Form::model($taskC, ['route' => 'task_l.store']) !!}
+            <div class="form-group">
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::select('status', ['済' => '済', '未' => '未', '保留' => '保留'], null, ['placeholder' => '']) !!}
+            </div>
+            {!! Form::submit('投稿') !!}
+        {!! Form::close() !!}
+        </div>
+        <div class="col-sm-2 col-sm-offset-2 col-md-2 col-md-offset-2 col-lg-3 col-lg-offset-3"></div>
+    </div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,13 +4,23 @@
 
     <h1>id: {{ $taskE->id }}のタスク編集ページ</h1>
     
-    {!! Form::model($taskE, ['route' => ['task_l.update', $taskE->id], 'method' => 'put']) !!}
-    
-        {!! Form::label('content', 'タスク') !!}
-        {!! Form::text('content') !!}
-        {!! Form::select('status', ['済' => '済', '未' => '未', '保留' => '保留']) !!}
-        {!! Form::submit('更新') !!}
-    
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+            {!! Form::model($taskE, ['route' => ['task_l.update', $taskE->id], 'method' => 'put']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク：') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス：') !!}
+                    {!! Form::select('status', ['済' => '済', '未' => '未', '保留' => '保留']) !!}
+                </div>
+                {!! Form::submit('更新') !!}
+            
+            {!! Form::close() !!}
+        </div>
+        <div class="col-sm-2 col-sm-offset-2 col-md-2 col-md-offset-2 col-lg-3 col-lg-offset-3"></div>
+    </div>
 
 @endsection
